d03c13d470c1b40e489ec865c12880e5f0f8de5d
marthamareal <marthamareal@gmail.com>
2018-07-02 10:32:59 +0300

    create test users

---
+++ project.py
@@ -0,0 +1,8 @@
+"""
+    List of static users we are going to use temporarily
+"""
+users = [
+    {"id": 1, "role": "admin", "email": "admin@gmail.com", "password": "admin"},
+    {"id": 2, "role": "moderator", "email": "moderator@gmail.com", "password": "moderator"},
+    {"id": 3, "role": "normal", "email": "normal@gmail.com", "password": "normal"},
+         ]
